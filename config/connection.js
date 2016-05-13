b4c01af397f25325e392bf9fe4dd309f9f764754
kellymersereau <kelly.mersereau@gmail.com>
2016-05-13 08:58:12 -0400

    final fixes

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,11 @@
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    port: 3000,
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'burgers_db'
-});
+var connection;
 
 if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
     connection = mysql.createConnection({
+        root: 3000,
         host: 'localhost',
         user: 'root',
         password: '',
